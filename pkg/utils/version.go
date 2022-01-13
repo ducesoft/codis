ac4ab60bd5b6961bb87c3f9ef859e4c3e2adf1d3
coyzeng <renjue.zeng@trustbe.cn>
2022-01-13 12:19:51 +0800

    dont ignore version

---
+++ pkg/utils/version.go
@@ -0,0 +1,6 @@
+package utils
+
+const (
+	Version = "2018-11-04 16:22:35 +0800 @de1ad026e329561c22e2a3035fbfe89dc7fef764 @3.2.2-12-gde1ad026"
+	Compile = "2022-01-05 09:55:50 +0800 by go version go1.17.5 darwin/amd64"
+)
